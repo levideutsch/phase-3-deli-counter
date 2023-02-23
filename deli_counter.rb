a2cd2df85c0c79d164e42026103d06e522ce71b9
levideutsch <levideutsch@live.com>
2023-02-23 17:38:36 -0500

    commit tree 5b2042e66f873ee7c40193a95f0ed360499158fc

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require 'pry'
+
+def line(array)
+   
+    if array.length <= 0
+        puts "The line is currently empty."
+    elsif array.length >= 0
+       blah = "The line is currently:"
+        array.map.with_index do |person, index|
+           blah << " #{index + 1}. #{person}"
+        end
+        puts blah
+    end 
+end
+
+def take_a_number(array, person) 
+    array << person
+    puts "Welcome, #{person}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+   if array.length <= 0
+        puts "There is nobody waiting to be served!"
+    elsif array.length >= 0
+         puts "Currently serving #{array[0]}."
+         array.shift()
+    end
+end
